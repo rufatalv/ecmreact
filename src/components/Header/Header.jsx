f8722e473f221de57ef05c58f3b75511c19a110f
Rufat Aliyev <rufat845@gmail.com>
2023-04-11 23:57:46 +0400

    Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -42,9 +42,7 @@ const Header = (props) => {
           <li>
             <Link to="/dashboard">Featured</Link>
           </li>
-          <li>
-            <a href="https://oauth.vk.com/authorize?client_id=6287487&scope=1073737727&redirect_uri=https://oauth.vk.com/blank.html&display=page&response_type=token&revoke=1">VK LOGIN</a>
-          </li>
+         
         </ul>
       </div>
       <div className="headerRight">
