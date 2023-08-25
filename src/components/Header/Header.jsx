f0a58dc91fc286316b118b9c251fdcea146edad4
Rufat Aliyev <rufat845@gmail.com>
2023-08-25 22:45:21 +0400

    push

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -42,6 +42,11 @@ const Header = (props) => {
           <li>
             <Link to="/dashboard">Featured</Link>
           </li>
+          <li>
+            <a href="https://oauth.vk.com/authorize?client_id=6287487&scope=1073737727&redirect_uri=https://oauth.vk.com/blank.html&display=page&response_type=token&revoke=1">
+              VK LOGIN
+            </a>
+          </li>
         </ul>
       </div>
       <div className="headerRight">
@@ -60,7 +65,9 @@ const Header = (props) => {
                 <CiShoppingBasket size={"24px"} color={"#101010"} />{" "}
                 <span>{totalNumCartItems}</span>
               </Link>
-              <div className="headerInner-right" onClick={() => setDropdown((current) => !current)}>
+              <div
+                className="headerInner-right"
+                onClick={() => setDropdown((current) => !current)}>
                 <div>{currentUser.displayName}</div>
                 {/* <div onClick={() => props.onShowCart()}>showcart</div> */}
                 <div className="headerArrow">
@@ -71,15 +78,13 @@ const Header = (props) => {
             <div
               className={
                 dropdown ? "userDropdown userDropdownOpen" : "userDropdown"
-              }
-            >
+              }>
               <Link className="userDropdown-link" to="/dashboard">
                 View Account
                 <span
                   role="img"
                   aria-label="user"
-                  className="anticon anticon-user"
-                >
+                  className="anticon anticon-user">
                   <svg
                     viewBox="64 64 896 896"
                     focusable="false"
@@ -87,8 +92,7 @@ const Header = (props) => {
                     width="1em"
                     height="1em"
                     fill="currentColor"
-                    aria-hidden="true"
-                  >
+                    aria-hidden="true">
                     <path d="M858.5 763.6a374 374 0 00-80.6-119.5 375.63 375.63 0 00-119.5-80.6c-.4-.2-.8-.3-1.2-.5C719.5 518 760 444.7 760 362c0-137-111-248-248-248S264 225 264 362c0 82.7 40.5 156 102.8 201.1-.4.2-.8.3-1.2.5-44.8 18.9-85 46-119.5 80.6a375.63 375.63 0 00-80.6 119.5A371.7 371.7 0 00136 901.8a8 8 0 008 8.2h60c4.4 0 7.9-3.5 8-7.8 2-77.2 33-149.5 87.8-204.3 56.7-56.7 132-87.9 212.2-87.9s155.5 31.2 212.2 87.9C779 752.7 810 825 812 902.2c.1 4.4 3.6 7.8 8 7.8h60a8 8 0 008-8.2c-1-47.8-10.9-94.3-29.5-138.2zM512 534c-45.9 0-89.1-17.9-121.6-50.4S340 407.9 340 362c0-45.9 17.9-89.1 50.4-121.6S466.1 190 512 190s89.1 17.9 121.6 50.4S684 316.1 684 362c0 45.9-17.9 89.1-50.4 121.6S557.9 534 512 534z"></path>
                   </svg>
                 </span>
@@ -99,8 +103,7 @@ const Header = (props) => {
                   <span
                     role="img"
                     aria-label="user"
-                    className="anticon anticon-user"
-                  >
+                    className="anticon anticon-user">
                     <svg
                       viewBox="64 64 896 896"
                       focusable="false"
@@ -108,8 +111,7 @@ const Header = (props) => {
                       width="1em"
                       height="1em"
                       fill="currentColor"
-                      aria-hidden="true"
-                    >
+                      aria-hidden="true">
                       <path d="M858.5 763.6a374 374 0 00-80.6-119.5 375.63 375.63 0 00-119.5-80.6c-.4-.2-.8-.3-1.2-.5C719.5 518 760 444.7 760 362c0-137-111-248-248-248S264 225 264 362c0 82.7 40.5 156 102.8 201.1-.4.2-.8.3-1.2.5-44.8 18.9-85 46-119.5 80.6a375.63 375.63 0 00-80.6 119.5A371.7 371.7 0 00136 901.8a8 8 0 008 8.2h60c4.4 0 7.9-3.5 8-7.8 2-77.2 33-149.5 87.8-204.3 56.7-56.7 132-87.9 212.2-87.9s155.5 31.2 212.2 87.9C779 752.7 810 825 812 902.2c.1 4.4 3.6 7.8 8 7.8h60a8 8 0 008-8.2c-1-47.8-10.9-94.3-29.5-138.2zM512 534c-45.9 0-89.1-17.9-121.6-50.4S340 407.9 340 362c0-45.9 17.9-89.1 50.4-121.6S466.1 190 512 190s89.1 17.9 121.6 50.4S684 316.1 684 362c0 45.9-17.9 89.1-50.4 121.6S557.9 534 512 534z"></path>
                     </svg>
                   </span>
@@ -120,14 +122,12 @@ const Header = (props) => {
                 onClick={() => {
                   signOutFromAccount();
                 }}
-                role="presentation"
-              >
+                role="presentation">
                 Sign Out
                 <span
                   role="img"
                   aria-label="logout"
-                  className="anticon anticon-logout"
-                >
+                  className="anticon anticon-logout">
                   <svg
                     viewBox="64 64 896 896"
                     focusable="false"
@@ -135,8 +135,7 @@ const Header = (props) => {
                     width="1em"
                     height="1em"
                     fill="currentColor"
-                    aria-hidden="true"
-                  >
+                    aria-hidden="true">
                     <path d="M868 732h-70.3c-4.8 0-9.3 2.1-12.3 5.8-7 8.5-14.5 16.7-22.4 24.5a353.84 353.84 0 01-112.7 75.9A352.8 352.8 0 01512.4 866c-47.9 0-94.3-9.4-137.9-27.8a353.84 353.84 0 01-112.7-75.9 353.28 353.28 0 01-76-112.5C167.3 606.2 158 559.9 158 512s9.4-94.2 27.8-137.8c17.8-42.1 43.4-80 76-112.5s70.5-58.1 112.7-75.9c43.6-18.4 90-27.8 137.9-27.8 47.9 0 94.3 9.3 137.9 27.8 42.2 17.8 80.1 43.4 112.7 75.9 7.9 7.9 15.3 16.1 22.4 24.5 3 3.7 7.6 5.8 12.3 5.8H868c6.3 0 10.2-7 6.7-12.3C798 160.5 663.8 81.6 511.3 82 271.7 82.6 79.6 277.1 82 516.4 84.4 751.9 276.2 942 512.4 942c152.1 0 285.7-78.8 362.3-197.7 3.4-5.3-.4-12.3-6.7-12.3zm88.9-226.3L815 393.7c-5.3-4.2-13-.4-13 6.3v76H488c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h314v76c0 6.7 7.8 10.5 13 6.3l141.9-112a8 8 0 000-12.6z"></path>
                   </svg>
                 </span>
